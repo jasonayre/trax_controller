6e2c0a2b655216a3062c3f99cbc6da4bd228ef8d
Jason Ayre <jayre@peer60.com>
2015-10-27 12:20:36 -0600

    i cant for the life of me remember what I thought this was doing, dumb down and reuse index action

--- lib/trax/controller/collection/searchable.rb
+++ lib/trax/controller/collection/searchable.rb
@@ -5,36 +5,11 @@ module Trax
         extend ::ActiveSupport::Concern
 
         def search
-          render :json => search_collection,
-                 :meta => collection_response_meta,
-                 :each_serializer => collection_serializer,
-                 :root => collection_root
-        end
-
-        def search_collection
-          @search_collection ||= begin
-            search_keys = self.class.scopes_configuration.keys
-
-            search_keys.inject(collection.all) do |result, key|
-              relation = params.has_key?("#{key}") ? result.__send__(key, params[key]) : result
-              relation
-            end.all
-          end
-        end
-
-        def collection_searchable_meta
-          @collection_pagination_meta ||= ::Trax::Controller::Collection::Pageable::PAGINATION_META_METHODS.inject({}) do |h, key|
-            h[key] = collection.__send__(key)
-            h
-          end
+          index(meta: collection_response_meta, **options)
         end
 
         def collection_response_meta
-          if collection_action?
-            super.merge!(:search_scopes => current_scopes)
-          else
-            super
-          end
+          super.merge!(:search_scopes => current_scopes)
         end
       end
     end
