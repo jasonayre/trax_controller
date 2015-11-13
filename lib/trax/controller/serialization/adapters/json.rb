48c9ff8956f4516626a73fa1f26d0ee0aa64a643
vyrak bunleang <vyrak.bunleang@gmail.com>
2015-11-13 11:03:11 -0700

    make serialization adapter recursively serialize associations

--- lib/trax/controller/serialization/adapters/json.rb
+++ lib/trax/controller/serialization/adapters/json.rb
@@ -8,35 +8,29 @@ module Trax
       module Adapters
         class Json < ActiveModel::Serializer::Adapter::Json
           def serializable_hash(options={})
-            is_collection? ? collection_serializable_hash(options) : resource_serializable_hash(options)
+            result = is_collection? ? collection_serializable_hash(options) : resource_serializable_hash(options)
+            @options[:nested] ? result : { root =>  result }
           end
 
           private
 
-          def collection_result
-            @collection_result ||= []
-          end
-
           def collection_serializable_hash(options={})
-            _result = collection_result + serializer.map { |s| FlattenJson.new(s).serializable_hash(options) }
-            { root =>  _result }
+            serializer.map { |s| FlattenJson.new(s).serializable_hash(options) }
           end
 
           def is_collection?
             serializer.respond_to?(:each)
           end
 
-          def resource_result
-            @resource_result ||= {}
-          end
-
           def resource_serializable_hash(options={})
-            cache_check(serializer){ resource_result.merge!(serializer.attributes(options)) }
-            serialize_resource_associations
-            { root => resource_result }
+            cached = cache_check(serializer){ serializer.attributes(options) }
+            resource_result = serialize_resource_associations
+            cached.merge(resource_result)
           end
 
           def serialize_resource_associations
+            resource_result = {}
+
             serializer.associations.each do |association|
               association_serializer = association.serializer
               association_options = association.options
@@ -45,16 +39,18 @@ module Trax
                 array_serializer = association_serializer
 
                 resource_result[association.key] = array_serializer.map do |item|
-                  cache_check(item) { FlattenJson.new(item).serializable_hash }
+                  cache_check(item) { self.class.new(item, :nested => true).serializable_hash }
                 end
               else
                 resource_result[association.key] = if association_serializer && association_serializer.object
-                  cache_check(association_serializer) { FlattenJson.new(association_serializer).serializable_hash }
+                  cache_check(association_serializer) { self.class.new(association_serializer, :nested => true).serializable_hash }
                 elsif association_options[:virtual_value]
                   association_options[:virtual_value]
                 end
               end
             end
+
+            resource_result
           end
         end
       end
--- spec/trax/serialization/adapters/json_spec.rb
+++ spec/trax/serialization/adapters/json_spec.rb
@@ -16,19 +16,20 @@ describe Trax::Controller::Serialization::Adapters::Json do
   let(:category_knives){ ::Category.new(:name => "knives") }
   let(:category_tools){ ::Category.new(:name => "tools", :subcategories => [category_screwdrivers, category_knives]) }
   let(:product){ ::Product.new(:name => "Generics Multi-Purpose Tool", :category => category_tools) }
-  let(:serializer){ ::NameProductSerializer.new(product) }
+  let(:root_key){ :root_key }
+  let(:serializer){ ::NameProductSerializer.new(product, :root => root_key) }
   subject(:serialized){ described_class.new(serializer).serializable_hash }
 
   it {
-    expected = { :product => {
+    expected = { root_key => {
       :name => product.name,
       :category => {
         :name => category_tools.name,
         :subcategories => [
           { :name => category_screwdrivers.name,
-            :subcategories => [{:name => category_flat_heads.name}]
+            :subcategories => [{:name => category_flat_heads.name, :subcategories => []}]
           },
-          {:name => category_knives.name}
+          {:name => category_knives.name, :subcategories => []}
         ]
       }
     }}
