e20bcf79432b9d435f3339c1fb6d1c086a682301
Jason Ayre <jayre@peer60.com>
2016-04-14 16:27:10 -0600

    add configuration, and autoload authorization related modules

--- lib/trax/controller.rb
+++ lib/trax/controller.rb
@@ -16,19 +16,32 @@ module Trax
     end
 
     autoload :Actions
+    autoload :Authorize
+    autoload :Authorization
+    autoload :Config
     autoload :Collection
     autoload :Engine
     autoload :Resource
     autoload :InheritResources
     autoload :ActionTypes
-    autoload :Engine
     autoload :Serialization
+
+    @configuration ||= ::Trax::Controller::Config.new
+
+    def self.configure(&block)
+      block.call(config)
+    end
 
     include ::Trax::Controller::InheritResources
     include ::Trax::Controller::Collection::Base
     include ::Trax::Controller::Resource::Base
     include ::Trax::Controller::Actions
     include ::Trax::Controller::ActionTypes
+
+    class << self
+      attr_accessor :configuration
+      alias_method :config, :configuration
+    end
   end
 end
 
