d35624c96ad02c4539b0a9edc943892d87fe15f0
Jason Ayre <jayre@peer60.com>
2016-04-18 14:54:08 -0600

    fix the way that we determine collection_action? or resource_action?

--- lib/trax/controller/action_types.rb
+++ lib/trax/controller/action_types.rb
@@ -2,27 +2,17 @@ module Trax
   module Controller
     module ActionTypes
       extend ::ActiveSupport::Concern
-      #todo: pretty janky but works for now
-
-      included do
-        class_attribute :action_types
-
-        self.action_types = Hashie::Mash.new({
-          :collection => [:index, :search],
-          :resource => [ :show, :delete, :update, :create, :login ]
-        })
-      end
 
       def current_action
         :"#{request.params["action"]}"
       end
 
       def collection_action?
-        self.class.action_types[:collection].include?(current_action)
+        !!get_collection_ivar
       end
 
       def resource_action?
-        self.class.action_types[:resource].include? current_action
+        !!get_resource_ivar
       end
     end
   end
