419e0c4c1b15ab1eee249b1e05c750d5794257a0
Jason Ayre <jayre@peer60.com>
2016-05-10 15:55:32 -0600

    remove puts

--- lib/trax/controller/collection/sortable.rb
+++ lib/trax/controller/collection/sortable.rb
@@ -10,9 +10,6 @@ module Trax
           def has_sort_scope(sort_scope_name, only: [:index, :search], type: :boolean, **options)
             SORT_DIRECTIONS.each do |dir|
               _sort_scope_name = :"#{sort_scope_name}_#{dir}"
-
-              puts _sort_scope_name
-
               has_scope(_sort_scope_name, only: only, type: type, **options)
             end
           end
