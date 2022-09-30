6a80764cc42809de0a4b2f294ddbc1899a743668
jasonayre <jasonayre@gmail.com>
2022-09-30 16:00:02 -0600

    fix params permitter

--- lib/trax/controller/permit_params_for.rb
+++ lib/trax/controller/permit_params_for.rb
@@ -17,17 +17,21 @@ module Trax
       def params_permitter_for_action
         action = params[:action]
         return self.trax_params_permitters[action.to_sym] if self.trax_params_permitters.key?(action.to_sym)
-        return self.trax_params_permitters[:save] if self.trax_params_permitters.key?(:save) && (action == 'create' || action == 'update')
+        return self.trax_params_permitters[:save] if self.trax_params_permitters.key?(:save) && (action == 'create' || action == 'update' || action == 'first_or_create')
         raise StandardError.new("No param permitter defined for action #{action}")
       end
 
       def build_resource_params
         _params = request.parameters[resource_request_name]
-        params_permitter_for_action.new(_params).to_hash
+        self.class.trax_params_permitters[action_name.to_sym].new(_params).to_serializable_hash
       end
 
       def build_resource
         get_resource_ivar || set_resource_ivar(end_of_association_chain.send(method_for_build, resource_params))
+      end
+
+      def update_resource(object, attributes)
+        object.update(attributes)
       end
 
       module ClassMethods
@@ -39,8 +43,10 @@ module Trax
         def permitted_params_for_save(&block)
           _update_klass = ::Trax::Core::NamedClass.new("#{self.name}::PermittedParamsForActionUpdate", Trax::Core::Types::Struct, &block)
           _create_klass = ::Trax::Core::NamedClass.new("#{self.name}::PermittedParamsForActionCreate", Trax::Core::Types::Struct, &block)
+          _first_or_create_klass = ::Trax::Core::NamedClass.new("#{self.name}::PermittedParamsForActionFirstOrCreate", Trax::Core::Types::Struct, &block)
           self.trax_params_permitters[:update] = _update_klass
           self.trax_params_permitters[:create] = _create_klass
+          self.trax_params_permitters[:first_or_create] = _first_or_create_klass
         end
       end
     end
